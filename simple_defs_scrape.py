bbcd6f559f91e3796a7b4965c3a020653788bc2e
Martin Breuss <martin-martin@users.noreply.github.com>
2018-03-13 13:28:35 +0700

    remove obsolete comments
    taking out the sample HTML structure to add it as a separate document. makes it easier to view and refer to from both scrape files

--- simple_defs_scrape.py
+++ simple_defs_scrape.py
@@ -24,79 +24,9 @@ def scrape(word):
   page_url = "http://www.wordreference.com/es/en/translation.asp?spen=" + word
   try:
     page = requests.get(page_url)
-    #print page
     soup = BeautifulSoup(page.text, "html.parser")
-    #print "#######################################"
-    #print html
     # creating a bucket for the simple definitions
     def_list = list()
-
-    ########## UNDERSTANDING THE DATA STRUCTURE ##########
-
-    # the definitions are nested in a <div> id='articleWRD'
-    # and its <table> class='WRD' elements, further nested like so:
-    #
-    # <tr class='wrtopsection'>
-    #   <td colspan='3' title='Principal Translations'>
-    #     <strong>Principal Translations</strong>
-    #   </td>
-    # </tr>
-    # <tr class='langHeader' style='font-size: 13px;text-decoration: underline;font-weight:bold;'>
-    #   <td class='FrWrd'>Spanish</td>
-    #   <td></td>
-    #   <td class='ToWrd'>English</td>
-    # </tr>
-    # <tr class='even' id='esen:18465'>
-    #   <td class='FrWrd' >
-    #     <strong>vamos</strong> <em class='tooltip POS2'>expr<span><i>expresión</i>: Expresiones idiomáticas, dichos, refranes y frases hechas de tres o más palabras ("Dios nos libre", "a lo hecho, pecho").</span></em>
-    #   </td>
-    #   <td> (para instar, urgir)</td>
-    #   <td class='ToWrd' >
-    #     let´s go, come on <em class='tooltip POS2'>interj<span><i>interjection</i>: Exclamation--for example, "Oh no!"  "Wow!"</span></em>
-    #   </td>
-    # </tr>
-    # <tr class='even'>
-    #   <td>&nbsp;</td>
-    #   <td colspan='2' class='FrEx'>Vamos, hay que darse prisa que se nos hace tarde.</td>
-    # </tr>
-
-    ###### IMPORTANT SECTIONS ######
-
-    # <tr class='wrtopsection'>
-    #   <td title='Principal Translations'>
-    #     <strong>________</strong>
-    #   </td>
-    # </tr>
-    # <tr class='langHeader'>
-    #   <td class='FrWrd'>Spanish</td>
-    #   <td></td>
-    #   <td class='ToWrd'>English</td>
-    # </tr>
-    # <tr id='esen:18465'>
-    #   <td class='FrWrd' >
-    #     <strong>vamos</strong>
-    #     <em class='tooltip POS2'>expr
-    #       <span>
-    #         <i>expresión</i>:
-    #           Expresiones idiomáticas, dichos, refranes y frases hechas
-    #           de tres o más palabras ("Dios nos libre", "a lo hecho, pecho").
-    #       </span>
-    #     </em>
-    #   </td>
-    #   <td> (para instar, urgir)</td>
-    #   <td class='ToWrd' >
-    #     let´s go, come on
-    #     <em class='tooltip POS2'>interj
-    #       <span>
-    #         <i>interjection</i>: Exclamation--for example, "Oh no!"  "Wow!"
-    #       </span>
-    #     </em>
-    #   </td>
-    # </tr>
-    # <tr>
-    #   <td></td>
-    #   <td class='FrEx'>Vamos, hay que darse prisa que se nos hace tarde.</td>
-    # </tr>
 
     # match all the sections with numbered word IDs
     all_defs = soup.findAll("tr", {"id" : regex})
@@ -111,7 +41,7 @@ def scrape(word):
       # TODO: this is erroneous. if the translation consists of more than one word,
       #       it slices incorrectly!!!!!! needs a fix.
 
-      # the definition is always the second entry <td> (see example HTML above)
+      # the definition is always the second entry <td> (see example HTML structure in 'sample.html')
       # it does NOT have a class or id associated, so I get it through slicing
       # it alsow starts with a whitespace char, which we don't need, so: lstrip()
       simple_def = d.findAll("td")[1].get_text().lstrip()
